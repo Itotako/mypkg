9c67a6e48acca51251881a1e5f566208e27b769e
Kaito Ito <s23c1016de@s.chibakoudai.jp>
2025-01-09 03:50:57 +0900

    correct

--- test/test.bash
+++ test/test.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Kaito Ito
 # SPDX-License-Identifier: BSD-3-Clause
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -15,13 +16,13 @@ source $dir/.bashrc
 # エラーチェック用関数
 ng() {
     echo "$1"
-    res=1
+    res=1  # エラー発生時にresを1に設定
 }
 
-res=0
+res=0  # 初期化
 
 # battery_state_publisher ノードをバックグラウンドで実行
-ros2 run mypkg battery_state_publisher &
+ros2 run mypkg battery_state_publisher & 
 NODE_PID=$!
 
 # 少し待機してからトピックリストを確認
@@ -46,9 +47,9 @@ if [ $? -ne 0 ]; then
 fi
 
 # トピックの出力を確認
-timeout 10 ros2 topic echo /battery_state > output_state.log &
+timeout 10 ros2 topic echo /battery_state > output_state.log & 
 ECHO_STATE_PID=$!
-timeout 10 ros2 topic echo /battery_level > output_level.log &
+timeout 10 ros2 topic echo /battery_level > output_level.log & 
 ECHO_LEVEL_PID=$!
 
 # 少し待機してから出力を終了
@@ -69,11 +70,15 @@ else
     ng "battery_level トピックの出力がありません"
 fi
 
-
 # 一時的なファイルを削除
 rm -f output_state.log output_level.log
 
 # 結果表示
-[ "$res" = 0 ] && echo "OK"
+if [ "$res" -eq 0 ]; then
+    echo "OK"
+else
+    echo "エラーが発生しました"
+fi
+
 exit "$res"
 
